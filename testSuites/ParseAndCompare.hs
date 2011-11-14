3b3b5e3e21df0b9543cd48a986e1d1868d746cd8
Malin <ahlberg.malin@gmail.com>
2011-11-14 17:10:03 +0100

    start on treebank

--- testSuites/ParseAndCompare.hs
+++ testSuites/ParseAndCompare.hs
@@ -1,4 +1,10 @@
-import Parser
+import Parsing
+import Data.Function
+import Control.Monad
+import Control.Arrow
+import System.Environment (getArgs)
+import System.Process
+import Prelude hiding (compare)
 
 main = do
   (file:arg) <- getArgs
@@ -6,8 +12,19 @@ main = do
   correct <- readFile "treebank.txt"
   have    <- readFile "tmp.tmp"
   compare correct have
-
-compare = zipWith rate `on` parseResult
-  where rate (i,n) (j,m) = undefined
+  rawSystem "rm" ["tmp.tmp"]
 
 
+compare = zipWithM_ rate `on` parseResult
+  where rate (i,n) (j,m) | i /= j = putStrLn $ color red 
+                                       $ "Not the same sentence! "++show i++" "++show j
+                         | n <  m = putStrLn $ color turkos 
+                                       $ "Sentence "++show i++" can be parsed in "++show m++" ways!"
+                                          ++ " (before only "++show m++")"
+
+                         | n == m = putStrLn $ color green $ "Sentence "++show i++" ok."
+                         | n >  m = putStrLn $ color red   $ "Sentence "++show i++" not ok."
+
+
+parseResult :: String -> [(String,Int)]
+parseResult = map (second read . read) . lines
---
+++ testSuites/Parsing.hs
@@ -0,0 +1,175 @@
+module Parsing where
+import System.Timeout
+import Data.Maybe
+import Data.Map hiding (map,null,filter)
+import Data.Ord
+import Data.Char
+import Data.List
+import Data.Function
+import Control.Concurrent
+import Control.Arrow
+import Control.Applicative
+import Control.Monad.State
+import PGF
+import XMLHelp hiding (words,parse)
+
+-- use with tee -a outfile
+data Parsed = Pars {t :: Tree, s :: String, n :: Int}
+  deriving Show
+
+data Count = C {ok :: Int, reject :: Int, total :: Int, lexErr :: Int, time :: Int
+               ,strict :: Bool , outputFile :: FilePath}
+type CState = StateT Count IO
+type Sent = (Id,String)
+-- 40 s, maybe too much
+timeLimit =  30*10^6
+
+run file strict out = do  
+  putStr "Parsing input file..."
+  sents <- mainF file (return . map getSentence)
+  putStr " Ok\nReading PGF..."
+  pgf <- readPGF pgfFile
+  putStrLn " Ok\n"
+  --absMap <- readAbbs 
+  let morpho = buildMorpho pgf lang
+  c <- execStateT (mapM_ (tryparse pgf morpho) (concat sents)) (newCount strict out)
+  print c
+  return ()
+
+tryparse :: PGF -> Morpho -> Sent -> CState ()
+tryparse pgf morpho sents = do 
+  count 
+  parse' pgf morpho sents
+
+parse' :: PGF -> Morpho -> Sent -> CState ()
+parse' pgf morpho (s,"") = addEmpty >> putEmptyMsg (s,"")
+parse' pgf morpho s = do
+  let fix     = fixPunctuation $ replaceNumbers s 
+      unknown = badWords morpho (snd fix)
+  strict <- gets strict
+  if null unknown then parseOk pgf fix
+        else putLexMsg s unknown strict >> addBadLex
+
+parseOk :: PGF -> Sent-> CState ()        
+parseOk pgf s = do
+  strict <- gets strict
+  let eval = if strict then ($!) else ($)
+  tree <- io $ timeout timeLimit $ return `eval` parse pgf lang textType (map toLower $ snd s)
+  maybe (addTime >> putTimeMsg s) (getBestTree s) tree
+
+
+badWords :: Morpho -> String -> [String]
+badWords morpho s = [w| w <- words s, null $ lookupMorpho morpho $ map toLower w]
+
+replaceNumbers :: Sent -> Sent
+replaceNumbers = second (unwords . map exchangeNum . words)
+  where exchangeNum w | any isNumber w = "1" 
+                      | otherwise   = w
+fixPunctuation :: Sent -> Sent
+fixPunctuation (n,s) | isAlpha (last s) = (n,s++" .")
+                     | otherwise        = (n,s)
+getBestTree :: Sent -> [Tree] -> CState ()
+getBestTree s tree = do
+  --let treelist = map (rank . toPars) tree
+  --let newTrees = map t $ sortBy (comparing n) treelist
+  io $ putMsg s tree
+  unless (null tree) addOk
+  strict <- gets strict
+  out    <- gets outputFile
+  io $ appendFile out (formatRes strict (s,tree)++"\n")
+
+addOk,addEmpty, addBadLex :: CState ()
+addOk     = modify $ \s -> s {ok = succ (ok s)}
+addEmpty  = modify $ \s -> s {reject = succ (reject s)}
+addBadLex = modify $ \s -> s {lexErr = succ (lexErr s)}
+addTime   = modify $ \s -> s {time = succ (time s)}
+count     = modify $ \s -> s {total = 1 + total s}
+
+putTimeMsg s   = output s blue   " was timed out" 
+putEmptyMsg s  = output s turkos " was rejected"  
+putLexMsg s ws strict | strict = output s red 
+                                  (" was not parsed (unknown words: "
+                                  ++unwords ws++")" )
+                      | otherwise =  io $ print (fst s,"0")
+
+output :: Sent -> Color -> String -> CState ()
+output s c str = do
+  io $ putStrLn $ show s ++ color c str 
+  io $ appendFile outFile $ "\n" ++ show s ++ str
+
+
+textType = fromJust $ readType "Text"
+uttType  = fromJust $ readType "Phr"
+lang     = fromJust $ readLanguage "BigTestSwe" --"BigSwe" --"BigNewSwe"
+pgfFile =  "../gf/BigTest.pgf" -- "../gf/Big.pgf" -- "BigNew.pgf"
+outFile = "testis.txt"
+
+
+toPars :: Tree -> Parsed
+toPars t = Pars t (showExpr [] t) 5
+
+-- guessing game...
+rank :: Parsed -> Parsed
+rank (Pars t s n) = Pars t s $ n + sum [i | (cat,i) <- rules, cat `isInfixOf` s]
+
+-- [(GFConstructor,Point)] The more points given the lower ranking
+rules :: [(String,Int)]
+rules = [ ("GenNP"  , 2),("ApposCN" ,  2),("MassNP"   ,  1),("NumCard"   ,  1),
+          ("UseCl"  ,1),("dropAtt", 1),("man_nn",  1)
+         ,("faar_nn", 1),("en_nn"   ,  1),("foer_nn"  ,  1),("mycken_nn" ,  1),("nyy_nn",  1)
+         ,("vill_ab", 1),("titt_nn" ,  1),("leva_nn"  ,  1)
+         ,("haer_nn", 1),("dehaer_N", -1),("dethaer_N", -1),("denhaer_N" , -1),("(Refl" , -1)
+         ,("PrepCN",  1)]
+         -- ReflVP and ReflGenVP rather than without 
+            
+io :: IO a -> CState a
+io = lift
+
+newCount :: Bool -> FilePath -> Count
+newCount = C 0 0 0 0 0
+
+instance Show Count where
+  show (C ok rej tot lex time _ _) 
+     = "*******\n Result: passed: "++show ok++"\t rejected: "
+       ++show rej++"\t timed out: "++show time++"\n\t of totally "
+       ++show tot++" sentences."++"\n"++show lex++" had unknown words"
+
+isStrict (x:xs) = ("S" `isPrefixOf` x)
+isStrict []     = False
+
+
+-------
+formatRes strict (s,[])  | strict =  show s ++ " was not Parsed"
+                         | otherwise = show (fst s,"0")
+formatRes strict (s,trs) | strict = show s ++ "("++show (length trs)++" trees)" 
+                                    ++ ":\n" ++ unlines (map (showExpr []) trs) 
+                         | otherwise = show (fst s,show $ length trs)
+
+
+-- Terminal color output
+type Color = Int
+
+color :: Color -> String -> String
+color c s = fgcol c ++ s ++ normal
+
+normal = "\ESC[0m"
+
+bold :: String -> String
+bold = ("\ESC[1m" ++)
+
+
+fgcol :: Int -> String
+fgcol col = "\ESC[0" ++ show (30+col) ++ "m"
+
+red = 1
+green = 2
+yellow = 3
+blue = 4    -- correct this!
+turkos = 5    -- correct this!
+
+putMsg :: Sent -> [Tree] -> IO ()
+putMsg s [] = putStrLn $ show s ++ color red " was not parsed"
+putMsg s ps = putStrLn $ show s ++ (color green " was parsed in "
+                                      ++show (length ps)++" ways")
+
+
