1fdf6fce42628aab2a951e45b4bb1d1972fe2fa7
Malin <ahlberg.malin@gmail.com>
2012-01-25 20:51:44 +0100

    pretty good ne

--- robust/ne/Main.hs
+++ robust/ne/Main.hs
@@ -1,12 +1,76 @@
-import XMLHelp hiding (words)
+import XMLHelp hiding (words,id)
 import Ne
+import PGF
 
+import Data.List
+import Data.Function
 import Control.Arrow
+import Control.Applicative
+
+{- gets 98.8% of the PNs
+   + there were 1035 not PNs (but may be correct anyway)
+   Without morpho-check (toLower all), get 91.6%
+   + there were 673 not PNs  
+   -}
 
 maini :: FilePath -> IO ()
 maini out = do
+  putStrLn "parse pgf"
+  pgf  <- readPGF "../../gf/Big.pgf"
+  putStrLn "building morpho"
+  let morpho = buildMorpho pgf (read "BigSwe")
+  putStrLn "extracting sentences"
   sent <- mainF tb (return . map getSentence')
-  let output = map (second isName) $ concat sent
+  putStrLn "finding names"
+  let output = map (second (isName morpho)) $ concat sent
   writeFile out $ unlines $ map show output
 
 tb = "../../Talbanken05_20060604/FPS/P.tiger.xml"
+
+test xs = do
+  putStrLn "parse pgf"
+  pgf  <- readPGF "../../gf/Big.pgf"
+  putStrLn "building morpho"
+  let morpho = buildMorpho pgf (read "BigSwe")
+  return $ isName morpho (words xs)
+
+testxml = do
+  sent <- mainF "100.xml" (return . map getSentenceTagged)
+  print sent
+
+evalNE :: FilePath -> IO ()
+evalNE out = do 
+  putStrLn "parse pgf"
+  pgf  <- readPGF "../../gf/Big.pgf"
+  putStrLn "building morpho"
+  let morpho = buildMorpho pgf (read "BigSwe")
+  putStrLn "extracting sentences"
+  sent <- concat <$> mainF tb (return . map getSentenceTagged)
+  putStrLn "finding names"
+  let output = map ((isName morpho). map snd . snd) sent -- differnte type now, with pos!
+  writeFile out $ sortResult $ eval $ compareT output sent
+
+compareT :: [[String]] -> [(Id,[(Tag,String)])] -> [(Tag,Tag,String)]
+compareT ntags tbtags = concat $ zipWith (\x (i,y) -> 
+                               zipWith (\a (b,c) -> (a,b,c)) x y) ntags tbtags
+
+eval :: [(Tag,Tag,String)] -> ([(Tag,String)],[String])
+eval ((nt,tb,str):xs) = let (tagged,untagged) = eval xs
+                        in  if nt =="X1" || nt == "X2" then ((tb,str):tagged,untagged)
+                            else if tb=="PN" then (tagged,str:untagged)
+                                 else (tagged,untagged)
+eval [] = ([],[])
+
+sortResult :: ([(Tag,String)],[String]) -> String
+sortResult (ns,pns) = 
+   let as = gather (take 2) (map fst ns)
+       bs = gather id pns
+       str1 = "Tagged as names:\n"++unlines (map format as)
+       str2 = "Untagged PNs ("++show (length pns)++"):\n" 
+               ++ unlines (map show bs)
+   in str1++"\n\n"++str2++"\nToo tagged:\n++"
+          ++unwords [s++" "++t | (t,s) <-ns, not ("PN" `isPrefixOf` t)]
+  where gather f = reverse . sortBy (compare `on` snd) 
+                 . map (\x -> (head x,length x)) . groupBy ((==) `on` f) 
+                 . sort
+        format (t,n) = show n ++ " tagged as "++t
--- robust/ne/Ne.hs
+++ robust/ne/Ne.hs
@@ -1,25 +1,52 @@
 module Ne where
 import Data.Char
+import PGF
+import Control.Arrow
 
-isName :: [String] -> [String]
-isName (x:xs)  = replace (x:xs) 
-isName []      = []
+{- Beginning of sentence? Look up in morpho and mark as name if not present?
+   Animate/inanimate? Look at rest of sentence, or make alternatives. (vote down GenNP when parsing)
+   Nominative/genitive? Put as both if ends with s?
+   -}
+isName :: Morpho -> [String] -> [String]
+isName m (x:xs)  | null (lookupMorpho m (lower x)) = replace (x:xs) -- first word not in dict -> is name
+                 | otherwise                       = (x:replace xs)
+{-= replace (lower x:xs) -}
 
-isNameStart :: String -> Bool
-isNameStart xs = isUpper (head xs) || isNameSpec xs
+isName m []      = []
+
+lower (x:xs) = (toLower x:xs)
+lower y      = y
+
+--isNameStart :: String -> Bool
+--isNameStart xs = isUpper (head xs) || isNameSpec xs
 
 isNameSpec = (`elem` ["von","af"])
 
 replace :: [String] -> [String]
-replace (x:y:xs) | isUpper (head x) && isCombiner y = "X" : replace (dropName xs)
-replace (x:xs)   | isNameSpec x     = "X" : dropName xs
-                 | isUpper (head x) = "X" : replace (dropName xs)
-                 | otherwise        = x : replace xs
+--replace (x:y:xs) | isUpper (head x) && isCombiner y =
+replace (x:xs)  | isNameSpec x  || looksNamish x = 
+       let (s,rest) = dropName ('-', xs)
+       in  gfName s : replace xs
+                | otherwise        = x : replace xs
 replace [] = []
 
-dropName (x:xs)   | isUpper (head x) = dropName xs
-                  | isNameSpec x     = dropName xs
-                  | otherwise        = x:xs
-dropName []  = []
+dropName :: (Char,[String]) -> (Char,[String])
+dropName (c,x:xs)   | isUpper (head x) = second ("":) $ dropName (last x,xs)
+                    | isNameSpec x     = second ("":) $ dropName ('-',xs)
+                    | otherwise        = (c,x:xs)
+dropName (c,[])  =  (c,[])
 
-isCombiner = (`elem` ["&","och"])
+isCombiner = (`elem` ["&"])
+
+looksNamish :: String -> Bool
+looksNamish s = isUpper (head s) && notPn s
+
+notPn = not . (`elem` ["Dig","Din","Dina","Du","Er","Era","Er","Eder"])
+
+gfName 's' = "X2"  -- when the name ends with an s, it might be genitive
+gfName _   = "X1"
+
+{-
+tagName :: Morpho -> [(Tag,String)] -> [String]
+tagName m  = isName 
+-}
