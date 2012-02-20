0a57d93f54f1591a383d28216b7a6d417fa64968
Malin <ahlberg.malin@gmail.com>
2012-02-20 22:17:15 +0100

    Intergrate - fancying up

--- robust/valens/Intergrate.hs
+++ robust/valens/Intergrate.hs
@@ -15,18 +15,20 @@ main = do
   writeFile "SmallerDict.gf" $ unlines keeps
 
 removeDuplicates :: Morpho -> PGF -> (String,String) -> String
-removeDuplicates morpho pgf (lem,line) | isVerb lem =
-  let lst = [(lemma,cat) | (lemma,an) <- lookupMorpho morpho (toStr lem)
-                 ,let cat = maybe "" (showType []) (functionType pgf lemma)
-                 ]
-      rem = null lst
-  in if trace ((toStr lem)++" gives "++show lst) rem then line else ""
-                                      | otherwise = line
+removeDuplicates morpho pgf (lem,line) | isVerb lem = removeVerb
+                                       | otherwise = line
+ where removeVerb =
+        let lst = [ (lemma,cat) 
+                  | (lemma,an) <- lookupMorpho morpho (toStr lem)
+                  , let cat = maybe "" (showType []) (functionType pgf lemma)
+                  ]
+            rem = null lst
+        in if trace ((toStr lem)++" gives "++show lst) rem then line else ""
    
 toStr = backTranslate . takeWhile (/='_')
 verb  = fromJust $ readType "V"
 dict  = "../../saldo/DictSwe.gf"
 pgfFile   = "ValLexAbs.pgf"
 isVerb = ("V" `isSuffixOf`)
-backTranslate str = foldl (flip ($)) str 
-                      [replace a b | (a,b) <- [("ae","ä"),("aa","å"),("oe","ö")]] 
+backTranslate str = foldr ($) str 
+  [replace a b | (a,b) <- [("ae","ä"),("aa","å"),("oe","ö")]] 
