7711e0e4bc0e7a7060f489dd9175f4074a505bee
Malin <ahlberg.malin@gmail.com>
2012-03-16 15:04:19 +0100

    Ids in NewerTest too

--- robust/chunk/NewerTest.hs
+++ robust/chunk/NewerTest.hs
@@ -19,73 +19,74 @@ import System.IO
 import System.TimeIt
 
 main = do hSetBuffering stdout LineBuffering
-          timeIt $ {-tryAll "hardtests.xml"  -}try "stest.xml"
+          timeIt $ tryAll "hardtests.xml"  --try "stest.xml"
+
+type ChunkTree = Tree (Id,String)
+
+tryAll fil = do
+  putStrLn $ "Start, reading saldo ..."
+  saldo <- getSaldo 
+  putStrLn $ "Reading pgf ... "
+  pgf <- readPGF pgfBigFile
+  putStrLn $ "Building morpho ... "
+  let morpho = buildMorpho pgf langBig
+  putStrLn $ "Parsing xml ... "
+  input <- splitFile . concat <$> Form.parseIdTree fil
+  pgfs <- mapM (extractDicts saldo morpho) [x | x <- zip input [0..]]  
+  mapM processPartition pgfs
+
+ where processAndWrite :: PGF -> Language -> (String,ChunkTree) -> IO ()
+       processAndWrite pgf lang (i,tree) = do
+          res <- parseText tree pgf lang startType
+          appendFile "newfancytest.txt" $ showRes (i,res) ++"\n"
+        where ziptree = getFirstWord $ fromTree tree
+
+       extractDicts :: Saldo -> Morpho -> ([(String,ChunkTree)],Int) -> IO ([String],[WorkingTree],Maybe (FilePath,Language))
+       extractDicts saldo morpho (input,i) = do
+          putStrLn $ "Processing the tree "
+          let ziptrees        = map (getFirstWord . fromTree . snd) input
+              (newsTrees,res) = runWriter (mapM (processTree True saldo morpho 0) ziptrees)
+              (lms,names)     = res
+          writeFile ("namesEx"++show i) $ formatNames names
+          lex <- mkLexMap 
+          pgfs <- mkDir lex lms i 
+          return (map fst input, newsTrees,pgfs)
+
+       processPartition :: ([String],[WorkingTree],Maybe (FilePath,Language)) -> IO ()
+       processPartition (ids,newsTrees,Just (pgf,newLang)) = do
+          putStrLn $ "using pgf file "++pgf
+          newPgf <- readPGF pgf 
+          probs  <- readProbabilitiesFromFile "ProbsNewSpec" newPgf
+          let goodPgf = setProbabilities probs newPgf
+          putStrLn $ "Parsing the tree "
+          mapM_ (processAndWrite goodPgf newLang) $ zip ids (map toTree newsTrees)
+
+       splitFile :: [(String,ChunkTree)] -> [[(String,ChunkTree)]]
+       splitFile = unfoldr (\xs -> if null xs then Nothing else Just (take 10 xs,drop 10 xs))
 
 
---tryAll fil = do
---  putStrLn $ "Start, reading saldo ..."
---  saldo <- getSaldo 
---  putStrLn $ "Reading pgf ... "
---  pgf <- readPGF pgfBigFile
---  putStrLn $ "Building morpho ... "
---  let morpho = buildMorpho pgf langBig
---  putStrLn $ "Parsing xml ... "
---  input <- splitFile . concat <$> Form.parseIdTree fil
---  pgfs <- mapM (extractDicts saldo morpho) [x | x <- zip input [0..]]  
---  mapM processPartition pgfs
---
--- where processAndWrite :: PGF -> Language -> (String,Tree String) -> IO ()
---       processAndWrite pgf lang (i,tree) = do
---          res <- parseText tree pgf lang startType
---          appendFile "newfancytest.txt" $ showRes (i,res) ++"\n"
---        where ziptree = getFirstWord $ fromTree tree
---
---       extractDicts :: Saldo -> Morpho -> ([(String,Tree String)],Int) -> IO ([String],[TreePos Full String],Maybe (FilePath,Language))
---       extractDicts saldo morpho (input,i) = do
---          putStrLn $ "Processing the tree "
---          let ziptrees        = map (getFirstWord . fromTree . snd) input
---              (newsTrees,res) = runWriter (mapM (processTree True saldo morpho 0) ziptrees)
---              (lms,names)     = res
---          writeFile ("namesEx"++show i) $ formatNames names
---          lex <- mkLexMap 
---          pgfs <- mkDir lex lms i 
---          return (map fst input, newsTrees,pgfs)
---
---       processPartition :: ([String],[TreePos Full String],Maybe (FilePath,Language)) -> IO ()
---       processPartition (ids,newsTrees,Just (pgf,newLang)) = do
---          putStrLn $ "using pgf file "++pgf
---          newPgf <- readPGF pgf 
---          probs  <- readProbabilitiesFromFile "ProbsNewSpec" newPgf
---          let goodPgf = setProbabilities probs newPgf
---          putStrLn $ "Parsing the tree "
---          mapM_ (processAndWrite goodPgf newLang) $ zip ids (map toTree newsTrees)
---
---       splitFile :: [(String,Tree String)] -> [[(String,Tree String)]]
---       splitFile = unfoldr (\xs -> if null xs then Nothing else Just (take 10 xs,drop 10 xs))
---
---
---tryMedium fil = do
---  putStrLn $ "Start, reading saldo ..."
---  lex <- getSaldo 
---  putStrLn $ "Reading pgf ... "
---  pgf <- readPGF "BigParse.pgf"
---  let plang = read "BigParseSwe"
---      morpho = buildMorpho pgf plang
---  putStrLn $ "Parsing xml ... "
---  input <- fmap concat $ Form.parseIdTree fil
---  putStrLn $ "Processing the tree "
---  let ziptrees        = map (getFirstWord . fromTree . snd) input
---      (newsTrees,res) = runWriter (mapM (processTree True lex morpho 0) ziptrees)
---      (lms,names)     = res
---  writeFile "namesEx" $ formatNames names
---  putStrLn $ "Lemmas requested: "++show (map head $ group $ sort lms)
---  putStrLn $ "Names exchanged "++show   (map head $ group $ sort names)
---  putStrLn $ "Parsing the trees "
---  mapM (processAndWrite pgf plang) $ zip (map fst input) (map toTree newsTrees)
--- where processAndWrite pgf lang (i,tree) = do
---         res <- parseText tree pgf lang startType
---         appendFile "reservtest.txt" $ showRes (i,res) ++"\n"
---        where ziptree = getFirstWord $ fromTree tree
+tryMedium fil = do
+  putStrLn $ "Start, reading saldo ..."
+  lex <- getSaldo 
+  putStrLn $ "Reading pgf ... "
+  pgf <- readPGF "BigParse.pgf"
+  let plang = read "BigParseSwe"
+      morpho = buildMorpho pgf plang
+  putStrLn $ "Parsing xml ... "
+  input <- fmap concat $ Form.parseIdTree fil
+  putStrLn $ "Processing the tree "
+  let ziptrees        = map (getFirstWord . fromTree . snd) input
+      (newsTrees,res) = runWriter (mapM (processTree True lex morpho 0) ziptrees)
+      (lms,names)     = res
+  writeFile "namesEx" $ formatNames names
+  putStrLn $ "Lemmas requested: "++show (map head $ group $ sort lms)
+  putStrLn $ "Names exchanged "++show   (map head $ group $ sort names)
+  putStrLn $ "Parsing the trees "
+  mapM (processAndWrite pgf plang) $ zip (map fst input) (map toTree newsTrees)
+ where processAndWrite pgf lang (i,tree) = do
+         res <- parseText tree pgf lang startType
+         appendFile "reservtest.txt" $ showRes (i,res) ++"\n"
+        where ziptree = getFirstWord $ fromTree tree
 
 
 try fil = do
